587da0c0f96faa478377e7c94ebaa71e1e323d70
yong <40924497@qq.com>
2023-09-13 18:09:10 +0800

    Initial Commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# MochaSwap V2
+
+This repository contains the core smart contracts for the MochaSwap V2 Protocol.
+
+## Deploy
+
+pnpm exec hardhat --network base deploy --tags AMM
---
+++ hardhat.config.ts
@@ -0,0 +1,58 @@
+import "dotenv/config";
+import "@nomiclabs/hardhat-ethers";
+import "@nomiclabs/hardhat-ethers";
+import "hardhat-deploy";
+
+const accounts = {
+  mnemonic:
+    process.env.MNEMONIC ||
+    "test test test test test test test test test test test junk",
+  accountsBalance: "990000000000000000000",
+};
+
+const config = {
+  networks: {
+    base: {
+      url: "https://developer-access-mainnet.base.org",
+      accounts,
+      chainId: 8453,
+      live: true,
+      saveDeployments: true,
+    },
+  },
+  namedAccounts: {
+    // e.g. ledger://0x18dd4e0Eb8699eA4fee238dE41ecF115e32272F8
+    deployer: process.env.LEDGER || { default: 0 },
+    alice: {
+      default: 1,
+    },
+    bob: {
+      default: 2,
+    },
+    carol: {
+      default: 3,
+    },
+    dev: {
+      default: 4,
+    },
+    feeTo: {
+      default: 5,
+    },
+  },
+
+  solidity: {
+    compilers: [
+      {
+        version: "0.6.12",
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 200,
+          },
+        },
+      },
+    ],
+  },
+};
+
+export default config;
